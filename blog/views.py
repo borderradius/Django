d9758c66a8251b476e22612de846e5e2b9775481
spacemonkey <spacemonkey@spacemonkeyui-MacBook-Pro.local>
2018-05-22 15:29:38 +0900

    게시되지 않은 글 목록 페이지

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post.pk)
     else:
@@ -49,7 +49,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post.pk)
     else:
@@ -57,4 +57,11 @@ def post_edit(request, pk):
 
     return render(request, 'blog/post_edit.html', {
         'form':form,
-    })    
+    })  
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+    return render(request, 'blog/post_draft_list.html',{
+        'posts': posts
+    })  
